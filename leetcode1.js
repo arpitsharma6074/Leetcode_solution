59dcccd791dc53de7afe838a0671fb910ee2278f
arpit sharma <itssharma6074@gmail.com>
2023-10-13 22:53:25 +0530

    solution of 1 in java

---
+++ leetcode1.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+     
+    for (let i =0;i<nums.length;i++){
+        let diff=target-nums[i];
+        for(let j=i+1;j<nums.length;j++){
+            if (nums[j]==diff){
+                
+                if (i!=j){
+                    return [i,j];
+                }
+              }
+         
+        }
+    }
+    
+};
